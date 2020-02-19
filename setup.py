7ccb40c08e8e42a6cd59e056699789af01fad8bd
Mar Gonzalez-Porta <mgonzalezporta@gmail.com>
2020-02-19 16:25:55 +0800

    add pandas as a dependency

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'multiqc'
+        'multiqc',
+        'pandas'
     ],
     entry_points={
         'multiqc.modules.v1': [
